e5bfe8eaed2e43be6ce3e1b632a35c11b93f2b5b
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-09-08 05:04:16 -0700

    C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /*
  * main - Entry point
  *
- * Description: Write a C program that prints exactly \"Programming is like building a multilingual puzzle, followed by a new line.
+ * Description: Write a C program that prints exactly a sentence followed by a new line.
  *
  * Return: Always 0 (Success)
  */
