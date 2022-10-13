f5f370548db1ec1bf3fb6c9bbb7dc0c0364dc807
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-10-13 15:58:45 -0700

    fix error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,31 +1,29 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
+
 /**
- * sum_them_all - sum all the parametes.
- * a blank line
- * @n: the number of parameters
- * Description: this function sum all the parameters)?
- * section header: the header of this function is variadic_functions.h)*
- * Return: this return the sum of the parameters.
+ * sum_them_all -entry point
+ * @n: number arguments
+ * Return: total addition
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num;
-	int sum;
-	unsigned int i;
-
-	i = 0;
+	/* Asigned pointer to get arguments */
+	va_list arg_p;
+	/* Count number arguments */
+	unsigned int count = 0;
+	/* Store add of arguments */
+	int add = 0;
 
 	if (n == 0)
 		return (0);
-	sum = 0;
-	va_start(num, n);
-	while (i < n)
-	{
-		sum = sum + va_arg(num, int);
-		i++;
-	}
-	va_end(num);
-	return (sum);
+	/* Start arguments and num arguments */
+	va_start(arg_p, n);
+	/* Loop to go through a pointer adding arguments */
+	for (count = 0; count < n; count++)
+		/* Add arguments */
+		add += va_arg(arg_p, int);
+	/* Free va_arg */
+	va_end(arg_p);
+	/* variable add */
+	return (add);
 }
