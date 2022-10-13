7b03b51de1f58ae840b7d2611726ecfeea940bfe
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-10-13 16:11:14 -0700

    fix error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,26 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * sum_them_all -entry point
- * @n: number arguments
- * Return: total addition
+ * sum_them_all - sum all arguements
+ * @n: number of arguements
+ * Return: 0 is n is 0 or return sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	/* Asigned pointer to get arguments */
-	va_list arg_p;
-	/* Count number arguments */
-	unsigned int count = 0;
-	/* Store add of arguments */
-	int add = 0;
+	va_list numbers;
+	int sum;
+	unsigned int i;
 
 	if (n == 0)
 		return (0);
-	/* Start arguments and num arguments */
-	va_start(arg_p, n);
-	/* Loop to go through a pointer adding arguments */
-	for (count = 0; count < n; count++)
-		/* Add arguments */
-		add += va_arg(arg_p, int);
-	/* Free va_arg */
-	va_end(arg_p);
-	/* variable add */
-	return (add);
+	sum = 0;
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (sum);
 }
