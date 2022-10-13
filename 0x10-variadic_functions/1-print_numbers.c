592cad94515039fec3fc21cb6f00ac1336311e20
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-10-13 16:06:07 -0700

    fix error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,18 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (separator == NULL)
-		return;
-
+	return;
 	va_start(valist, n);
-
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, const unsigned int));
-
-		if (i != (n - 1))
-			printf("%s", separator);
+	printf("%d", va_arg(valist, const unsigned int));
+	if (i != (n - 1))
+	printf("%s", separator);
 	}
 	printf("\n");
-
 	va_end(valist);
 }
