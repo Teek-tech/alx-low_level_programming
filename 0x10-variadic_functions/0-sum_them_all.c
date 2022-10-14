033df227ab4cbbe4dad4a98f9638442d29126811
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-10-14 02:19:24 -0700

    fix error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,25 +5,24 @@
 /**
  * sum_them_all - sum all the parametes.
  * @n: the number of parameters
- * Description: this function sum all the parameters
  * 
  * Return: this return the sum of the parameters.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num;
+	va_list args;
 	unsigned int i, sum = 0;
 	
 	if (n == 0)
 		return (0);
 
-	va_start(num, n);
+	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(num, const unsigned int);
+		sum += va_arg(args, const unsigned int);
 	}
-	va_end (num);
+	va_end(args);
 	return (sum);
 }
