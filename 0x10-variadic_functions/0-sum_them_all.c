39da92831fee7628bfd60c6f9ad9c7e4d5beb89f
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-10-13 17:03:34 -0700

    fix error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,16 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdlib.h>
+#include "variadic_functions.h"
+
 /**
  * sum_them_all - sum all the parametes.
-(* a blank line
- *@n: the number of parameters
-* Description: this function sum all the parameters)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return: this return the sum of the parameters.
-*/
+ * a blank line
+ * @n: the number of parameters
+ * Description: this function sum all the parameters)?
+ * section header: the header of this function is variadic_functions.h)*
+ * Return: this return the sum of the parameters.
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list num;
@@ -23,10 +25,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(num, n);
 
 	for (i = 0; i < n; i++)
-	{
 		x = x + va_arg(num, int);
-		i++;
-	}
 	va_end(num);
 	return (x);
 }
