b6f6941e2129d83457b8fd56096ac79a25308db8
Tochukwu Odeme <tochuks.chris@gmai..com>
2022-12-12 21:14:34 -0800

    fix error

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -25,7 +25,7 @@ int find(char *str, char c);
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
-int div(int a, int b);
+/**int div(int a, int b);*/
 int mod(int a, int b);
 
 #endif
